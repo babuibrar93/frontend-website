8f558bbff9a9d65e2d5e53371d13a98cec811a4c
babuibrar93 <51041999+babuibrar93@users.noreply.github.com>
2020-10-12 03:59:58 -0700

    first commit

---
+++ function.js
@@ -0,0 +1,9 @@
+let header = document.querySelector('.header');
+let menu = document.querySelector('.menu');
+
+menu.addEventListener('click', function() {
+    header.classList.toggle('menu-open');
+})
+
+
+
